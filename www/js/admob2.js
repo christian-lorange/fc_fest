e30c42325f132343411dbc1c4db217163c9ca810
clorange <lorange.christian@gmail.com>
2019-02-08 13:29:36 -0700

    no message

--- www/js/admob2.js
+++ www/js/admob2.js
@@ -1,15 +1,15 @@
 var admobid = {}
 if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
   admobid = {
-   banner: 'ca-app-pub-7061349157136298/7728721211',
-   interstitial: 'ca-app-pub-7061349157136298/8625077904',
-    //  banner: 'ca-app-pub-3940256099942544/6300978111',  //test id
-    //  interstitial: 'ca-app-pub-3940256099942544/1033173712', // test id
+  //banner: 'ca-app-pub-7061349157136298/7728721211',
+   //interstitial: 'ca-app-pub-7061349157136298/8625077904',
+      banner: 'ca-app-pub-3940256099942544/6300978111',  //test id
+      interstitial: 'ca-app-pub-3940256099942544/1033173712', // test id
   }
 } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
   admobid = {
-    banner: 'ca-app-pub-7061349157136298/7733177375',
-    interstitial: 'ca-app-pub-7061349157136298/1865186299',
+    //banner: 'ca-app-pub-7061349157136298/7733177375',
+    //interstitial: 'ca-app-pub-7061349157136298/1865186299',
   }
 }
 
@@ -33,20 +33,7 @@ document.addEventListener('deviceready', function() {
     admob.interstitial.show()
 
 
-    //Adding Push Notifications
-    // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("c8e5d878-daff-4659-abaa-28acb58fc70f")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-
-
+   
 
   }
 
@@ -70,3 +57,18 @@ document.addEventListener('admob.interstitial.events.CLOSE', function(event) {
 
   admob.interstitial.prepare()
 })
+
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("c8e5d878-daff-4659-abaa-28acb58fc70f")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
