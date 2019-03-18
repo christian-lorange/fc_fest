33be2bb3f200fcdc4afddf0e05bbc022027b8704
clorange <lorange.christian@gmail.com>
2019-03-18 17:05:45 -0600

    Adding 2019 content

--- www/js/mapresources.js
+++ www/js/mapresources.js
@@ -15,47 +15,360 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 var stations = {
   "type": "FeatureCollection",
-  "features": [{
-    "type": "Feature",
-    "properties": {
+  "features": [{"type": "Feature","properties": {
+      "STATION": "Aggie Theatre",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07652,40.58527]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Art Lab Fort Collins",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07489,40.58884]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Avogadros Number",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07927,40.57969]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The Colorado Room",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07657,40.57855]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Crooked Stave",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07653,40.58949]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Downtown Artery",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07418,40.58875]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Drunken Monkey",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07762,40.58587]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The East Coast",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07513,40.58865]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Elliots Martini Bar",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07438,40.58841]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Equinox Brewing",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07585,40.58625]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The Exchange",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07649,40.58976]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Fort Collins Museum of Discovery - Exhibit Hall",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07796,40.59349]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Fort Collins Museum of Discovery - Otter Box Digital Dome Theater",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07796,40.59349]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "High Point Bar",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07649,40.58804]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Hodis Half Note",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.0776,40.58839]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Illegal Petes",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07454,40.588]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Magic Rat",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07406,40.58767]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The Mayor of Old Town",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07406,40.58767]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Metro Urban Food & Booze",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07751,40.58642]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Moes Original Bar B Que",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.0776,40.58862]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The Music District - Living Room",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07766,40.57873]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The Music District - Media Library",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07766,40.57873]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "New Belgium Brewing Co. - Outside",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.06687,40.59341]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "New Belgium Brewing Co. - Tasting Room",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.06687,40.59341]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Odell Brewing Co.",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.06025,40.5889]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The Pizza Press",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07653,40.58949]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Pour Brothers Community Tavern",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07462,40.58829]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Prost Brewing Co. & Biergarten",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.074634,40.5884014]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "R Bar and Lounge",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.0766,40.57789]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The Rec Room",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07636,40.58758]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The Rickshaw Live",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.076622,40.5814705]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Scrumpys Hard Cider Bar and Pub",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07749,40.58949]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Sound Off Silent Disco at Old Town Square",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07596,40.58799]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Surfside 7",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.0747113,40.5886455]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Tap and Handle",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07763,40.58381]
+    }
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Tonys Bar & Rooftop",
+      "ST_CODE": "MGRP",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-105.07653,40.58478]
+    }
+  },
+ {"type": "Feature","properties": {
       "STATION": "Washingtons",
       "ST_CODE": "MGRP",
     },
     "geometry": {
       "type": "Point",
-      "coordinates": [-105.07789, 40.589516]
+      "coordinates": [-105.07793,40.58944]
     }
-  }, {
-    "type": "Feature",
-    "properties": {
-      "STATION": "Equinox Brewing",
-      "ST_CODE": "MGV1",
+  },
+ {"type": "Feature","properties": {
+      "STATION": "The Whiskey",
+      "ST_CODE": "MGRP",
     },
     "geometry": {
       "type": "Point",
-      "coordinates": [-105.075875,40.586343]
+      "coordinates": [-105.07653,40.5849]
     }
-  }, {
-    "type": "Feature",
-    "properties": {
-      "STATION": "The Aggie Theater",
-      "ST_CODE": "VICO",
+  },
+ {"type": "Feature","properties": {
+      "STATION": "Wolverine Farm Letterpress & Publick House",
+      "ST_CODE": "MGRP",
     },
     "geometry": {
       "type": "Point",
-      "coordinates": [-105.076611, 40.585262]
+      "coordinates": [-105.07334,40.59068]
     }
-  }, {
-    "type": "Feature",
-    "properties": {
-      "STATION": "Odell Brewing",
-      "ST_CODE": "MGBH",
-    },
-    "geometry": {
-      "type": "Point",
-      "coordinates": [-105.06332179999998, 40.589475]
-    }
-  }]
+  }
+
+
+  ]
 };
 
 var markerLayer = L.geoJson(stations, {
@@ -79,7 +392,7 @@ selector.onAdd = function(map) {
   //create div container
   var div = L.DomUtil.create('div', 'mySelector');
   //create select element within container (with id, so it can be populated later)
-  div.innerHTML = '<select id="marker_select" style="background-color: #135A32; color:white"><option value="init">Find Venue</option></select>';
+  div.innerHTML = '<select id="marker_select" style="background-color: #fcc60f; color:white"><option value="init">Find Venue</option></select>';
   return div;
 };
 
