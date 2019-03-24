338499498095050d715e1a2beb73e1b998c57966
DESKTOP-R7E62OP\Christian L'Orange <lorange.christian@gmail.com>
2019-03-24 10:40:02 -0600

    Updated images

--- www/js/admob2.js
+++ www/js/admob2.js
@@ -4,7 +4,7 @@ if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
   //banner: 'ca-app-pub-7061349157136298/7728721211',
    //interstitial: 'ca-app-pub-7061349157136298/8625077904',
       banner: 'ca-app-pub-3940256099942544/6300978111',  //test id
-      interstitial: 'ca-app-pub-3940256099942544/1033173712', // test id
+   // interstitial: 'ca-app-pub-3940256099942544/1033173712', // test id
   }
 } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
   admobid = {
@@ -21,12 +21,12 @@ document.addEventListener('deviceready', function() {
   })
   admob.banner.prepare()
 
-  admob.interstitial.config({
-    id: admobid.interstitial,
-    isTesting: false,
-    autoShow: false,
-  })
-  admob.interstitial.prepare()
+ // admob.interstitial.config({
+ //   id: admobid.interstitial,
+ //   isTesting: false,
+ //   autoShow: false,
+  //})
+  //admob.interstitial.prepare()
 
   document.getElementById('showAd').disabled = true
   document.getElementById('showAd').onclick = function() {
