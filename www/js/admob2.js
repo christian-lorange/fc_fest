37f04d21b100b11506bdb0041e3a23862f768c70
clorange <lorange.christian@gmail.com>
2019-04-02 14:53:30 -0600

    no message

--- www/js/admob2.js
+++ www/js/admob2.js
@@ -1,25 +1,25 @@
-var admobid = {}
-if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
-  admobid = {
+//var admobid = {}
+//if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
+//  admobid = {
   //banner: 'ca-app-pub-7061349157136298/7728721211',
    //interstitial: 'ca-app-pub-7061349157136298/8625077904',
    //   banner: 'ca-app-pub-3940256099942544/6300978111',  //test id
    // interstitial: 'ca-app-pub-3940256099942544/1033173712', // test id
-  }
-} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
-  admobid = {
+//  }
+//} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
+//  admobid = {
     //banner: 'ca-app-pub-7061349157136298/7733177375',
     //interstitial: 'ca-app-pub-7061349157136298/1865186299',
-  }
-}
+//  }
+//}
 
-document.addEventListener('deviceready', function() {
-  admob.banner.config({
-    id: admobid.banner,
-    isTesting: false,
-    autoShow: false,
-  })
-  admob.banner.prepare()
+//document.addEventListener('deviceready', function() {
+//  admob.banner.config({
+//    id: admobid.banner,
+//    isTesting: false,
+//    autoShow: false,
+//  })
+//  admob.banner.prepare()
 
  // admob.interstitial.config({
  //   id: admobid.interstitial,
@@ -28,35 +28,35 @@ document.addEventListener('deviceready', function() {
   //})
   //admob.interstitial.prepare()
 
-  document.getElementById('showAd').disabled = true
-  document.getElementById('showAd').onclick = function() {
-    admob.interstitial.show()
+//  document.getElementById('showAd').disabled = true
+//  document.getElementById('showAd').onclick = function() {
+//    admob.interstitial.show()
 
 
    
 
-  }
+ // }
 
-}, false)
+//}, false)
 
-document.addEventListener('admob.banner.events.LOAD_FAIL', function(event) {
-  console.log(event)
-})
+//document.addEventListener('admob.banner.events.LOAD_FAIL', function(event) {
+//  console.log(event)
+//})
 
-document.addEventListener('admob.interstitial.events.LOAD_FAIL', function(event) {
-  console.log(event)
-})
+//document.addEventListener('admob.interstitial.events.LOAD_FAIL', function(event) {
+//  console.log(event)
+//})
 
-document.addEventListener('admob.interstitial.events.LOAD', function(event) {
-  console.log(event)
-  document.getElementById('showAd').disabled = false
-})
+//document.addEventListener('admob.interstitial.events.LOAD', function(event) {
+//  console.log(event)
+//  document.getElementById('showAd').disabled = false
+//})
 
-document.addEventListener('admob.interstitial.events.CLOSE', function(event) {
-  console.log(event)
+//document.addEventListener('admob.interstitial.events.CLOSE', function(event) {
+//  console.log(event)
 
-  admob.interstitial.prepare()
-})
+//  admob.interstitial.prepare()
+//})
 
 
 document.addEventListener('deviceready', function () {
